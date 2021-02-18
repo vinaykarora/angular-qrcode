1e068d16fb77ccb74df01b42fd52c6122c21fa6f
Vinay Arora <vinay.arora7171@hotmail.com>
2021-02-18 23:46:21 +0530

    Generate QR Code

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-qrcode';
+  elementType = NgxQrcodeElementTypes.URL;
+  correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+  value = 'Reach me at: https://vinayaroratech.com/';
+
+}
